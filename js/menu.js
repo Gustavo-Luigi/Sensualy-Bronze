1ffecb8eebdf2358fec7d90cfafb0fa8265aa193
Gustavo <gustavo_-_luigi@hotmail.com>
2020-11-18 00:05:45 -0300

    Animações, melhorias na UI e codificação JavaScript

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,26 @@
-let menu = document.getElementById('logo-nav-bar');
+class Menu {
+  static toggleMenu(){
+    let menu = document.getElementById('hamburguer-menu');
+    document.getElementById('logo-nav-bar').classList.toggle('change');
+  }
+  static getCurrentPage(){
+    const currentPage = window.location.pathname.split("/").pop();
+    const linkList = document.querySelectorAll('.menu-link');
+    
+    linkList.forEach((link) => {
+      if(currentPage == link.getAttribute('href')){
+        link.parentElement.classList.add('current-page');
+      }
+    })
+  }
+}
 
-menu.addEventListener('click', () => {
-  menu.classList.toggle('change');
-})
+
+document.addEventListener('click', Menu.toggleMenu);
+document.addEventListener('DOMContentLoaded', Menu.getCurrentPage);
+
+// window.addEventListener('resize', () => {
+//   console.log(window.innerWidth);
+// })
+
+
