251f4688c913f7944ff55f69f207ba7b48b6fe9d
Gustavo <gustavo_-_luigi@hotmail.com>
2020-11-20 02:18:26 -0300

    Pequenas alterações no scss e js

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,21 @@
-class Menu {
-  static toggleMenu(){
-    document.getElementById('logo-nav-bar').classList.toggle('change');
-  }
-  static getCurrentPage(){
-    const currentPage = window.location.pathname.split("/").pop();
-    const linkList = document.querySelectorAll('.menu-link');
-    
-    linkList.forEach((link) => {
-      if(currentPage == link.getAttribute('href')){
-        link.parentElement.classList.add('current-page');
-      }
-    })
-  }
+
+function highlightCurrentPage(){
+  const currentPage = location.pathname.split("/").pop();
+  const linkList = document.querySelectorAll('.menu-link');
+  
+  linkList.forEach( link => {
+    if(currentPage == link.getAttribute('href')){
+      link.parentElement.classList.add('current-page');
+    }
+  })
 }
 
 let menu = document.getElementById('hamburguer-menu');
 
-menu.addEventListener('click', Menu.toggleMenu);
-document.addEventListener('DOMContentLoaded', Menu.getCurrentPage);
+menu.addEventListener('click', () => {
+  document.getElementById('logo-nav-bar').classList.toggle('change');
+});
+document.addEventListener('DOMContentLoaded', highlightCurrentPage);
 
 // window.addEventListener('resize', () => {
 //   console.log(window.innerWidth);
