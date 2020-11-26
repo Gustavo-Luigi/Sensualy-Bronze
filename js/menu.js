be10d274940fd269f069174c951e369512e84552
Gustavo <gustavo_-_luigi@hotmail.com>
2020-11-26 18:52:16 -0300

    CSS da galeria e ajuste da imagem de fundo

--- js/menu.js
+++ js/menu.js
@@ -30,13 +30,12 @@ window.onscroll = function() {
   // console.log("Atual " + window.pageYOffset);
   let menuIsOpen = document.getElementById('logo-nav-bar').classList.contains('change');
   let currentScrollPos = window.pageYOffset;
-  
+
   if(currentScrollPos > minScrollpos && !menuIsOpen){
     if(prevScrollpos > currentScrollPos) {
       navBar.style.top = '40px';
     } else {
       navBar.style.top = '-200px';
-  
     }
   }
   prevScrollpos = currentScrollPos;
