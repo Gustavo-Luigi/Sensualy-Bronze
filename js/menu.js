29bd43ba178896fed9e69c1863f2d158ecb17462
Gustavo-Luigi <gustavo_-_luigi@hotmail.com>
2021-02-17 16:17:20 -0300

    Refatoração do Javascript

--- js/menu.js
+++ js/menu.js
@@ -1,109 +1,24 @@
-class Menu {
+// Hide and show navbar
+let prevScrollpos = window.pageYOffset;
+let minScrollpos = 175;
+let navBar = document.querySelector(".logo-nav-bar-container");
 
-  constructor(navBar, navBarContainer, hamburguer, changeClass, menuLink) {
-    this.navBar = document.querySelector(navBar);
-    this.navBarContainer = document.querySelector(navBarContainer);
-    this.hamburguer = document.querySelector(hamburguer)
-    this.menuLink = document.querySelectorAll(menuLink);
-    this.changeClass = changeClass;
-    this.prevScrollPos = window.pageYOffset;
-  }
-  highlightCurrentPage(highlightClassName) {
+window.onscroll = function() {
 
-    const currentPage = location.pathname.split("/").pop();
-    const linkList = this.menuLink;
-    
-    linkList.forEach( link => {
-      if(currentPage == link.getAttribute('href')){
-        link.parentElement.classList.add(highlightClassName);
-      }
-    })
-  }
+  let menuIsOpen = document.getElementById('logo-nav-bar').classList.contains('change');
+  let currentScrollPos = window.pageYOffset;
 
-  toggleMenu() {
-    this.navBar.classList.toggle(this.changeClass);
-  }
-
-  toggleNavOnScroll(minScrollPos, showPosition, hidePosition) {
-
-    let menuIsOpen = this.navBar.classList.contains(this.changeClass);
-    let currentScrollPos = window.pageYOffset;
-
-    if(currentScrollPos > minScrollPos && !menuIsOpen){
-      if(this.prevScrollPos > currentScrollPos) {
-        this.navBarContainer.style.top = `${showPosition}px`;
-      } else {
-        this.navBarContainer.style.top = `${hidePosition}px`;
-      }
+  if(currentScrollPos > minScrollpos && !menuIsOpen){
+    if(prevScrollpos > currentScrollPos) {
+      navBar.style.top = '40px';
+    } else {
+      navBar.style.top = '-200px';
     }
-    this.prevScrollPos = currentScrollPos;
   }
+  prevScrollpos = currentScrollPos;
 }
 
-
-let menu = new Menu('#logo-nav-bar', '.logo-nav-bar-container', '#hamburguer-menu', 'change', '.menu-link');
-
-menu.highlightCurrentPage('current-page');
-
-// let prevScrollpos = window.pageYOffset;
-
-window.onscroll = () => {
-  menu.toggleNavOnScroll(175, 40, -200);
-}
-
-document.getElementById('hamburguer-menu').addEventListener('click', () => {
-  menu.toggleMenu();
+// Menu toggle
+menu.addEventListener('click', () => {
+  document.getElementById('logo-nav-bar').classList.toggle('change');
 });
-
-
-
-
-
-
-
-
-// hide and show navbar
-// let prevScrollpos = window.pageYOffset;
-// let minScrollpos = 175;
-// let navBar =document.querySelector(".logo-nav-bar-container")
-
-// window.onscroll = function() {
-//   // console.log("Anterior: " + prevScrollpos);
-//   // console.log("Atual " + window.pageYOffset);
-//   let menuIsOpen = document.getElementById('logo-nav-bar').classList.contains('change');
-//   let currentScrollPos = window.pageYOffset;
-
-//   if(currentScrollPos > minScrollpos && !menuIsOpen){
-//     if(prevScrollpos > currentScrollPos) {
-//       navBar.style.top = '40px';
-//     } else {
-//       navBar.style.top = '-200px';
-//     }
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-// window.addEventListener('resize', () => {
-//   console.log(window.innerWidth);
-// })
-
-
-
-
-
-// **********************************
-
-// function highlightCurrentPage(){
-//   const currentPage = location.pathname.split("/").pop();
-//   const linkList = document.querySelectorAll('.menu-link');
-  
-//   linkList.forEach( link => {
-//     if(currentPage == link.getAttribute('href')){
-//       link.parentElement.classList.add('current-page');
-//     }
-//   })
-// }
-
-// menu.addEventListener('click', () => {
-//   document.getElementById('logo-nav-bar').classList.toggle('change');
-// });
